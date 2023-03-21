b232283080aae149b4cec8843627a7b122e90c3f
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-03-21 16:59:29 -0700

    Titles

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,46 @@
 
 // Your Code Here
+
+
+
+async function retrieveBook() {
+let retrieve = await fetch('http://localhost:3001/listBooks',{
+})
+let books = await retrieve.json();
+console.log(books)
+
+
+
+ books.forEach(adminBooks)
+
+}
+
+ 
+ function adminBooks(book) {
+    let adminContainer = document.getElementById('admin')
+    adminContainer.innerHTML += `
+       <div>
+        <ul>
+           <li>${book.title} </li> 
+           </ul>
+        </div>
+    `
+}
+retrieveBook()
+adminBooks()
+
+/*async function listBooks() {
+    await fetch('http://localhost:3001/listBooks', {
+       method: 'GET',
+     headers: 'application/json',
+      body: JSON.stringify ({
+ 'quantity': document.getElementsByid('books').value
+         }),
+     })
+     
+ };
+
+ const sbutton = document.querySelector('saveButton');
+
+ sbutton.addEventListener('click', listBooks); */
+ 
--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
 async function main() {
-
+    
     let response = await fetch('http://localhost:3001/listBooks')
     let books = await response.json()
+    console.log(books)
 
     books.forEach(renderBook)
 }
